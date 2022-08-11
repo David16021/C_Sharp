0fdae512ca2af4c97aa128c7c811480a2f793339
David16021 <david6021@gmail.com>
2022-08-10 23:42:02 -0500

    Comparacion de cadenas

---
+++ ComparacionCadenas/ComparacionCadenas/Program.cs
@@ -0,0 +1,15 @@
+﻿string nombre = "Harold Perez";
+Console.WriteLine(nombre == "HarOld Perez");
+Console.WriteLine(nombre.Equals("Harold Perez", StringComparison.OrdinalIgnoreCase));
+if(nombre == "HarOld Perez")
+    Console.WriteLine("El nombre: " + nombre + " es igual a Harold Perez");
+else
+    Console.WriteLine("El nombre: " + nombre + " no es igual a HarOld Perez");
+if (nombre.Equals("HAROLD PEREZ", StringComparison.OrdinalIgnoreCase))
+    Console.WriteLine("El nombre: " + nombre + " es igual a HAROLD PEREZ");
+else
+    Console.WriteLine("El nombre: " + nombre + " no es igual a HAROLD PEREZ");
+DateTime fecha = DateTime.Now;
+Console.WriteLine(fecha.ToShortDateString());
+Console.WriteLine(fecha.ToLongDateString());
+Console.WriteLine(fecha.ToString("yyyy-MM-dd HH:mm:ss"));
