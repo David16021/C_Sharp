2ae5ed973660a49bd15878396d774789d1440cf4
David16021 <david6021@gmail.com>
2022-08-13 23:46:17 -0500

    Registros

---
+++ AplicacionPedidos/LibPedidos/Registros.cs
@@ -0,0 +1,12 @@
+﻿public record recProductos
+{
+    public int idProducto { get; set; }
+    public string Descripcion { get; set; }
+    public string CodigoBarras { get; set; }
+    public recProductos(int id, string descripcion, string codigobarras)
+    {
+        this.idProducto = id;
+        this.Descripcion = descripcion;
+        this.CodigoBarras = codigobarras;
+    }
+}
---
+++ AplicacionPedidos/LibPedidos/clsProductos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibPedidos
+{
+    public class clsProductos
+    {
+        public int idProducto { get; set; }
+        public string Descripcion { get; set; }
+        public string CodigoBarras { get; set; }
+
+        public override string ToString()
+        {
+            return $"idProducto: {idProducto.ToString()}" + 
+                $"Descripcion:  {Descripcion} " +
+                $"Codigo Barras: {CodigoBarras}";
+        }
+    }
+}
