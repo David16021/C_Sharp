b1eb4635e2a5c7c990244c487c519307bc5ca5f3
David16021 <david6021@gmail.com>
2022-08-14 00:51:49 -0500

    Creacion de menu a partir de parametros

---
+++ AplicacionPedidos/AplicacionPedidos/FuncionesPrincipales.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionPedidos
+{
+    public static class FuncionesPrincipales
+    {
+
+        public static void EjemploCliente()
+        {
+            clsClientes cliente;
+            cliente = new clsClientes();
+            cliente.idCliente = 1;
+            cliente.NombreCompleto = "harold perez";
+            cliente.Rfc = "hehm10165";
+            Console.WriteLine(cliente.ToString());
+        }
+        public static void EjemploRecordProducto()
+        {
+            clsProductos Producto = new clsProductos();
+            Producto.idProducto = 1;
+            Producto.Descripcion = "Ref Manzana 600 ML";
+            Producto.CodigoBarras = "0001";
+            clsProductos Producto2 = new clsProductos();
+            Producto2 = Producto;
+            Producto2.CodigoBarras = "0";
+            recProductos rProducto = new recProductos(1, "Ref Manzana 600 ML", "0001");
+            recProductos rProducto2 = rProducto with { CodigoBarras = "0" };
+            Console.WriteLine("Producto: " + Producto);
+            Console.WriteLine("Producto2: " + Producto2);
+            Console.WriteLine("rProducto: " + rProducto);
+            Console.WriteLine("rProducto2: " + rProducto2);
+            if (Producto == Producto2)
+                Console.WriteLine("Los objetos son iguales");
+            else
+                Console.WriteLine("Los objetos son diferentes");
+            if (rProducto == rProducto2)
+                Console.WriteLine("Los registros son iguales");
+            else
+                Console.WriteLine("Los registros son diferentes ");
+        }
+    }
+}
--- AplicacionPedidos/AplicacionPedidos/Program.cs
+++ AplicacionPedidos/AplicacionPedidos/Program.cs
@@ -1,32 +1,24 @@
-﻿using LibPedidos;
-
-//clsClientes cliente;
-//cliente = new clsClientes();
-//cliente.idCliente = 1;
-//cliente.NombreCompleto = "Harold Perez";
-//cliente.Rfc = "HEHM10165";
-//Console.WriteLine(cliente.ToString());
-
-clsProductos Producto = new clsProductos();
-Producto.idProducto = 1;
-Producto.Descripcion = "Ref Manzana 600 ML";
-Producto.CodigoBarras = "0001";
-clsProductos Producto2 = new clsProductos();
-Producto2 = Producto;
-Producto2.CodigoBarras = "0";
-
-recProductos rProducto = new recProductos(1, "Ref Manzana 600 ML", "0001");
-recProductos rProducto2 = rProducto with { CodigoBarras = "0"};
-Console.WriteLine("Producto: " + Producto);
-Console.WriteLine("Producto2: " + Producto2);
-Console.WriteLine("rProducto: " + rProducto);
-Console.WriteLine("rProducto2: " + rProducto2);
-
-if (Producto == Producto2)
-    Console.WriteLine("Los objetos son iguales");
-else
-    Console.WriteLine("Los objetos son diferentes");
-if (rProducto == rProducto2)
-    Console.WriteLine("Los registros son iguales");
-else
-    Console.WriteLine("Los registros son diferentes ");
+﻿global using LibPedidos;
+using AplicacionPedidos;
+int opcion = 0;
+do
+{
+    Console.WriteLine("Menu de opciones");
+    Console.WriteLine();
+    Console.WriteLine("1. Ejemplo de cliente");
+    Console.WriteLine("2. Ejemplo de record de producto");
+    Console.WriteLine("3. Ejemplo de paso de párametros a un Método");
+    Console.WriteLine("20. Salir");
+    opcion = Convert.ToInt32(Console.ReadLine());
+    switch (opcion)
+    {
+        case 1:
+            FuncionesPrincipales.EjemploCliente();
+            break;
+        case 2:
+            FuncionesPrincipales.EjemploRecordProducto();
+            break;
+        default:
+            break;
+    }
+} while (opcion != 20);
