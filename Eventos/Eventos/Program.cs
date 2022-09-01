91c87615308947b0dc00ccdc983bc873d8433f7b
David16021 <david6021@gmail.com>
2022-08-31 22:37:27 -0500

    EventHandler con expresiones lambda

--- Eventos/Eventos/Program.cs
+++ Eventos/Eventos/Program.cs
@@ -4,6 +4,9 @@ int opcion = 0;
 do
 {
     Console.WriteLine("1. Eventos con delegados");
+    Console.WriteLine("2. Eventos con EventHandler");
+    Console.WriteLine("3. Eventos con delegados y expesiones lambda");
+    Console.WriteLine("4. Eventos con EventHandler y expesiones lambda");
     Console.WriteLine("10. Salir");
     opcion = Convert.ToInt32(Console.ReadLine());
     switch (opcion)
@@ -13,10 +16,31 @@ do
             repo.ReportaAvance += MuestraAvance;
             repo.ObtenTodos();
             break;
+        case 2:
+            clsRepositorioEventHandler repo2 = new clsRepositorioEventHandler();
+            repo2.ReportaAvance += MuestraAvance2;
+            repo2.ObtenTodos();
+            break;
+        case 3:
+            clsRepositorioDelegado repo3 = new clsRepositorioDelegado();
+            repo3.ReportaAvance += (int i ) => { Console.Write(i.ToString() + " ");  };
+            repo3.ObtenTodos();
+            break;
+        case 4:
+            clsRepositorioEventHandler repo4 = new clsRepositorioEventHandler();
+            repo4.ReportaAvance +=
+                (object? _, Argumentos e) => { Console.Write(e.Valor.ToString() + " "); };
+            repo4.ObtenTodos();
+            break;
         default:
             break;
     }
 } while (opcion != 10);
+
+void MuestraAvance2(object? sender, Argumentos e)
+{
+    Console.Write(e.Valor.ToString() + " ");
+}
 
 void MuestraAvance(int valor)
 {
---
+++ Eventos/Eventos/clsRepositorioEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventos
+{
+    public class Argumentos : EventArgs
+    {
+        public int Valor { get; set; }
+    }
+    public class clsRepositorioEventHandler
+    {
+        public event EventHandler<Argumentos> ReportaAvance;
+        public void ObtenTodos()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(250);
+                ReportaAvance(this, new Argumentos { Valor = i + 1 });
+            }
+        }
+    }
+
+}
